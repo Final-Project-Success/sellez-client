037d701635382374e6ab1758fed71019d754488e
FianFebryIspianto <hendryfebry01@gmail.com>
2022-12-30 16:08:07 +0700

    done install-tailwind

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -6,6 +6,10 @@ export default function HomePage(params) {
       <Link to={"/chat"} class="nav-link waves-effect" href="#">
         chat
       </Link>
+      {/* color red tailwind */}
+      <h1 className="text-3xl font-bold underline text-red-500 hover:text-red-700">
+        Hello world!
+      </h1>
     </>
   );
 }
