a5dc93dd7ed0150e01620a831d7af9aa79e66ad5
FianFebryIspianto <hendryfebry01@gmail.com>
2023-01-05 21:38:10 +0700

    create rtk api done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import { Provider } from "react-redux";
+import { store } from "./app/store.js";
 
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { productsApi } from "./features/apiSlice";
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={store}>
+      <ApiProvider api={productsApi}>
+        <RouterProvider router={router} />;
+      </ApiProvider>
+    </Provider>
+  );
 }
 
 export default App;
