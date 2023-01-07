b828b974e6e0b80afe68b90de70bb2f28e17a078
Adrian1414 <adrianlie.al04@gmail.com>
2023-01-07 15:41:45 +0700

    delete product done

--- admin/src/components/ProductRow.js
+++ admin/src/components/ProductRow.js
@@ -1,8 +1,29 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCanArrowUp } from '@fortawesome/free-solid-svg-icons'
-
+import { useDispatch} from 'react-redux'
+import { deleteProduct } from "../stores/actions/product";
+import Swal from 'sweetalert2'
 
 export default function ProductRow({product, i}) {
+  const dispatch = useDispatch()
+  
+  const onDelete = (id) => {
+    console.log('masuk ondelete');
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        dispatch(deleteProduct(id));
+        Swal.fire("Deleted!", "Product has been deleted.", "success");
+      }
+    });
+  };
     return(
         <>
           <tr>
@@ -24,8 +45,8 @@ export default function ProductRow({product, i}) {
                     </td>
                     <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
                     
-                    onClick={(e) => {
-                      console.log('nyoba delete');
+                    onClick={() => {
+                      onDelete(product.id)
                     }}
                     >
                     <FontAwesomeIcon icon={faTrashCanArrowUp} style={{marginLeft: 5}} className='cursor-pointer' size="lg" color='blue' 
--- admin/src/stores/actions/product.js
+++ admin/src/stores/actions/product.js
@@ -1,16 +1,34 @@
-import axios from "axios"
-let baseUrl = 'http://localhost:4000'
+import axios from "axios";
+let baseUrl = "http://localhost:4000";
 
 export function fetchProducts() {
-    return async(dispatch) => {
-        try {
-            const { data } = await axios({
-                method: 'GET',
-                url: `${baseUrl}/products`
-            })
-            dispatch({type: 'products/fetchAll', payload: data})
-        } catch (error) {
-            console.log(error);
-        }
+  return async (dispatch) => {
+    try {
+      const { data } = await axios({
+        method: "GET",
+        url: `${baseUrl}/products`,
+      });
+      dispatch({ type: "products/fetchAll", payload: data });
+    } catch (error) {
+      console.log(error);
     }
+  };
+}
+
+export function deleteProduct(id) {
+  return async (dispatch) => {
+    try {
+      await axios({
+        method: "DELETE",
+        url: `${baseUrl}/products/${id}`,
+        headers: {
+          access_token: localStorage.access_token,
+        },
+      });
+
+      dispatch(fetchProducts());
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
