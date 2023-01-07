b97ae56940ca0c3343f796f29e265e8460263c1b
Adrian1414 <adrianlie.al04@gmail.com>
2023-01-07 17:21:08 +0700

    detail page

---
+++ client/src/pages/DetailPage.jsx
@@ -0,0 +1,44 @@
+export default function DetailPage() {
+    return(
+        <>
+
+<section className="text-gray-700 body-font overflow-hidden bg-white">
+  <div className="container px-5 py-24 mx-auto">
+    <div className=" mx-auto flex flex-wrap">
+      <img alt="ecommerce" style={{width: 300, marginLeft: 470}}  className="object-center rounded border border-gray-200" src="https://cdn.shopify.com/s/files/1/0516/0760/1336/products/Voila_1_f179f0c5-9c5e-43a6-9fe6-b5a252f55f5b_1000x.jpg?v=1642647113"/>
+      <div className=" w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+        <h2 className="text-sm title-font text-gray-500 tracking-widest">Air Jordan 1</h2>
+        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">Air Jordan 1 Bred Patent</h1>
+        <div className="flex mb-4">
+        
+       
+
+        
+                </div>
+        <p className="leading-relaxed">The Air Jordan 1 Retro High OG 'Patent Bred' treats the iconic colorway to a glossy makeover. Aside from the shoe's patent leather construction, the essential design DNA remains intact. The upper pairs basic black paneling with contrasting hits of Varsity Red on the toe box, Swoosh, heel overlay and collar flap. A woven Nike tag adorns the black nylon tongue, while a Wings logo is stamped on the lateral collar. The high-top rides on a sturdy rubber cupsole, enhanced with an Air-sole unit encapsulated in lightweight polyurethane.</p>
+        <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
+          <div className="flex">
+            <span className="mr-3">Color</span>
+            <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
+            <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
+            <button className="border-2 border-gray-300 ml-1 bg-red-500 rounded-full w-6 h-6 focus:outline-none"></button>
+          </div>
+        
+
+        </div>
+        <div className="flex">
+          <span className="title-font font-medium text-2xl text-gray-900">Rp. 5.000.000</span>
+          <button className="flex ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-300 ease-linear transition-all duration-200 rounded">CheckOut</button>
+          <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+            <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+              <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+        </>
+    )
+}
